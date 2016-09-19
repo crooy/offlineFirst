7f026e58a950eac170e0bbd65cb657b9e4e90127
Ronald Crooy <r.a.crooy@gmail.com>
2016-09-19 22:24:00 +0200

    almost ready

--- sw-dynamic.js
+++ sw-dynamic.js
@@ -1,56 +1,19 @@
-
-// Files required to make this app work offline
-const REQUIRED_FILES = [
-  'style.css',
-  'index.html',
-  '/', // Separate URL than index.html!
-  'app.js',
-  'imgs/noimage.jpg',
-  'bower_components/fullpage.js/jquery.fullPage.css',
-  'bower_components/jquery/dist/jquery.min.js',
-  'bower_components/fullpage.js/jquery.fullPage.min.js',
-  'run_prettify.js'
-];
-
-self.addEventListener('install', function (event) {
-  // Perform install step:  loading each required file into cache
-  event.waitUntil(
-    caches.open('dependencies-cache')
-    .then(function (cache) {
-      // Add all offline dependencies to the cache
-      return cache.addAll(REQUIRED_FILES);
-    })
-    .then(function () {
-      // At this point everything has been cached
-      return self.skipWaiting();
-    })
-  );
-});
-
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
-      // Cache hit - return the response from the cached version
-      if (response) {
-        return response;
-      }
+    caches.open('dynamic-caches')
+    .then(function (cache) {
 
-      //else try the dynamic cache
-      return caches.open('dynamic-caches').then(function(cache) {
-        return cache.match(event.request).then(function (response) {
-          return response || fetch(event.request).then(function(response) {
-            cache.put(event.request, response.clone());
-            return response;
+      return cache.match(event.request)
+      .then(function (cachedResponse) {
+
+        if (cachedResponse) return cachedResponse;
+
+        return fetch(event.request)
+          .then(function (onlineResponse) {
+            cache.put(event.request, onlineResponse.clone());
+            return onlineResponse;
           });
-        });
       });
-    }).catch(function (err) {
-      // If both fail, show a generic fallback:
-      if (event.request.url.endsWith(".jpg") || event.request.url.endsWith(".png")) {
-        return caches.match('imgs/noimage.jpg');
-      }
-      throw err;
     })
   );
 });
--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ var REQUIRED_FILES = [
   'bower_components/fullpage.js/jquery.fullPage.css',
   'bower_components/jquery/dist/jquery.min.js',
   'bower_components/fullpage.js/jquery.fullPage.min.js',
-  'run_prettify.js'
+  'run_prettify.js',
+  'imgs/noimage.jpg'
 ];
 
 self.addEventListener('install', function(event) {
@@ -30,16 +31,22 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function(cachedResponse) {
         // Cache hit - return the response from the cached version
-        if (response) {
-          return response;
+        if (cachedResponse) {
+          return cachedResponse;
         }
 
         // Not in cache - return the result from the live server
         // `fetch` is essentially a "fallback"
-        return fetch(event.request);
-      }
-    )
+        return fetch(event.request)
+          .catch(function (err) {
+            // If both fail, show a generic fallback:
+            if (event.request.url.endsWith(".jpg") || event.request.url.endsWith(".png")) {
+              return caches.match('imgs/noimage.jpg');
+            }
+            throw err;
+          });
+      })
   );
 });
